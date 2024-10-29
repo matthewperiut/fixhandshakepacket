64f12a1a12a2584d3d066f63f4a0eb9bf5d1cbdf
matthewperiut <matthewperiut@gmail.com>
2024-10-28 20:52:47 -0400

    Initial commit

---
+++ src/main/java/com/matthewperiut/fixhandshakepacket/FixHandshakePacket.java
@@ -0,0 +1,9 @@
+package com.matthewperiut.fixhandshakepacket;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FixHandshakePacket implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
